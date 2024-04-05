65c829b1a3b3f3f70c4048b86627a0697a4cb3fb
Gizmo <77913129+Habbobba@users.noreply.github.com>
2024-04-05 01:40:07 -0400

    Update goldfish.js

--- public/GoldFish/js/goldfish.js
+++ public/GoldFish/js/goldfish.js
@@ -1,22 +1,22 @@
 function updateStatic() {
-  $.ajax({
-      url: '/api/hotel',
-      dataType: "text",
-      type: 'GET',
-      success: function(data){
-        var parsed_data = JSON.parse(data);
-        $('#onlinecount').html(parsed_data.hotel.online);
-      }
-    });
+	$.ajax({
+		url: '/api/hotel',
+		dataType: "text",
+		type: 'GET',
+		success: function(data) {
+			var parsed_data = JSON.parse(data);
+			$('#onlinecount').html(parsed_data.hotel.online);
+		}
+	});
 }
 $(document).ready(function(e) {
-    $.ajaxSetup({
-        cache:true
-    });
-    setInterval(function() {
-      updateStatic();
-    }, 15000);
-    $( "#onlinecount").click(function() {
-      updateStatic();
-    });
-  });
+	$.ajaxSetup({
+		cache: true
+	});
+	setInterval(function() {
+		updateStatic();
+	}, 15000);
+	$("#onlinecount").click(function() {
+		updateStatic();
+	});
+});
